750719c051dcd48004e5dd139daf06480737b1d5
SpringMT <today.is.sky.blue.sky@gmail.com>
2022-06-23 03:19:30 +0900

    fix: fix SEGV for streaming decompress with GC.compact

--- spec/zstd-ruby-streaming-decompress_spec.rb
+++ spec/zstd-ruby-streaming-decompress_spec.rb
@@ -16,5 +16,21 @@ RSpec.describe Zstd::StreamingDecompress do
       expect(result).to eq(str)
     end
   end
+
+  describe 'streaming decompress + GC.compact' do
+    it 'shoud work' do
+      # str = SecureRandom.hex(150)
+      str = "foo bar buzz" * 100
+      cstr = Zstd.compress(str)
+      stream = Zstd::StreamingDecompress.new
+      result = ''
+      result << stream.decompress(cstr[0, 5])
+      result << stream.decompress(cstr[5, 5])
+      GC.compact
+      result << stream.decompress(cstr[10..-1])
+      expect(result).to eq(str)
+    end
+  end
+
 end
 
