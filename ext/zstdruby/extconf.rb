331a4ab5bcd0915f11f6f01d0b9e6f9c221f3789
Spring_MT <makoto.haruyama@knowledgework.com>
2025-07-06 13:48:48 +0900

    Fix symbol collision with other native gems using libzstd
    - Hide all ZSTD symbols by default using -fvisibility=hidden
    - Use exported_symbols_list on macOS to explicitly control exported symbols
    - Only export Init_zstdruby function required for Ruby extension initialization
    - Add RUBY_FUNC_EXPORTED macro to Init_zstdruby function
    
    This resolves symbol collision issues when using zstd-ruby alongside other
    gems that dynamically link to system libzstd (e.g., rdkafka-ruby).
    
    Fixes #102

--- ext/zstdruby/extconf.rb
+++ ext/zstdruby/extconf.rb
@@ -2,8 +2,13 @@ require "mkmf"
 
 have_func('rb_gc_mark_movable')
 
-$CFLAGS = '-I. -O3 -std=c99 -DZSTD_STATIC_LINKING_ONLY -DZSTD_MULTITHREAD -pthread -DDEBUGLEVEL=0'
+$CFLAGS = '-I. -O3 -std=c99 -DZSTD_STATIC_LINKING_ONLY -DZSTD_MULTITHREAD -pthread -DDEBUGLEVEL=0 -fvisibility=hidden -DZSTDLIB_VISIBLE=\'__attribute__((visibility("hidden")))\' -DZSTDLIB_HIDDEN=\'__attribute__((visibility("hidden")))\''
 $CPPFLAGS += " -fdeclspec" if CONFIG['CXX'] =~ /clang/
+
+# macOS specific: Use exported_symbols_list to control symbol visibility
+if RUBY_PLATFORM =~ /darwin/
+  $LDFLAGS += " -exported_symbols_list #{File.expand_path('exports.txt', __dir__)}"
+end
 
 Dir.chdir File.expand_path('..', __FILE__) do
   $srcs = Dir['**/*.c', '**/*.S']
